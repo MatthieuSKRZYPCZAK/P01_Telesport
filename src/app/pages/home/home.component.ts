aa8c5577eab3ef4d062ad7ffe67a0adbef36e164
Matthieu <46696317+MatthieuSKRZYPCZAK@users.noreply.github.com>
2024-11-13 10:06:31 +0100

    Added the OnDestroy lifecycle hook to manage component cleanup on destruction. Added a Subscription property to HomeComponent to handle the olympics$ observable subscription.

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,5 +1,5 @@
-import {Component, OnInit} from '@angular/core';
-import {Observable, of} from 'rxjs';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {Observable, of, Subscription} from 'rxjs';
 import {OlympicService} from 'src/app/core/services/olympic.service';
 import {Olympic} from "../../core/models/Olympic";
 
@@ -8,21 +8,27 @@ import {Olympic} from "../../core/models/Olympic";
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
   public olympics$: Observable<Olympic[]> = of([]);
   protected totalJOs: number = 0;
   protected totalCountries: number = 0;
+  private subscription!: Subscription;
 
   constructor(private olympicService: OlympicService) {
   }
 
   ngOnInit(): void {
     this.olympics$ = this.olympicService.getOlympics();
-    this.olympics$.subscribe((countries: Olympic[]) => {
+    this.subscription = this.olympics$.subscribe((countries: Olympic[]) => {
       if (countries) {
         this.totalCountries = countries.length;
         this.totalJOs = countries.reduce((sum: number, country) => sum + country.participations.length, 0);
       }
     });
   }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
+
 }
